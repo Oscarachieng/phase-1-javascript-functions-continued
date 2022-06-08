9e9c3ddc6ae537df32427c1d0689bfb8b4b0b171
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-08 20:13:24 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+ console.log(saturdayFun("worship"))
+
+ function mondayWork(activity= "go to the office") {
+     return `This Monday, I will ${activity}.`
+ }
+
+ console.log(mondayWork());
+
+function wrapAdjective(visualFlair="*") {
+    return function(adjective= "special") {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+}
+console.log(wrapAdjective("!!")("humerous"))
